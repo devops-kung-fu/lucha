93af2ed6aa544a07bf70a094d8fe4a56902cccfa
DJ Schleen <djschleen@gmail.com>
2021-07-12 09:23:26 -0600

    More test cases

--- lib/filesystem_test.go
+++ lib/filesystem_test.go
@@ -90,3 +90,22 @@ func TestFileSystem_AbsoluteSearchPath(t *testing.T) {
 	}
 	assert.Contains(t, fs.AbsoluteSearchPath(), "/code/lucha/lib")
 }
+
+func Test_shouldIgnoreDir(t *testing.T) {
+	fs := FileSystem{
+		fs:         afero.NewMemMapFs(),
+		SearchPath: ".",
+	}
+	fs.Afero().Mkdir(".git", 0644)
+	fi, _ := fs.Afero().ReadDir(fs.SearchPath)
+	assert.Len(t, fi, 1)
+
+	dir := fi[0]
+
+	shouldIgnore := shouldIgnoreDir(fs, dir, "")
+	assert.True(t, shouldIgnore)
+
+	fs.IncludeGit = true
+	shouldIgnore = shouldIgnoreDir(fs, dir, "")
+	assert.False(t, shouldIgnore)
+}
